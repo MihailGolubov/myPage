f9d57a05972e2c6b4c4b8bad98db496d3ad43d17
MihaiGolub <mihai.golubov@gmail.om>
2019-01-09 21:45:04 +0200

    vue

--- src/assets/scripts/works.js
+++ src/assets/scripts/works.js
@@ -14,7 +14,7 @@ window.onload=function(){
         });
 
     const parallaxContainer = document.querySelector('.parallax');
-    const layers = Array.from(parallaxContainer.chilldren);
+    const layers = Array.from(parallaxContainer.children);
     const moveLayers = e => {
     const initialX = (window.innerWidth / 2) - e.pageX;
     const initialY = (window.innerHeight / 2) - e.pageY;
