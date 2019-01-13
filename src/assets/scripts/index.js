0f1451766c6d4af0f5a820cdae08755b658d879b
MihaiGolub <mihai.golubov@gmail.om>
2019-01-13 02:04:36 +0200

    flip

---
+++ src/assets/scripts/index.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    const parallaxContainer = document.querySelector('.parallax');
+    const layers = Array.from(parallaxContainer.children);
+    const moveLayers = e => {
+        const initialX = (window.innerWidth / 2) - e.pageX;
+        const initialY = (window.innerHeight / 2) - e.pageY;
+    
+        layers.forEach((layer, i) => {
+            const divider = i / 100;
+            const positionX = initialX * divider;
+            const positionY = initialY * divider;
+            const bottomPosition = (window.innerHeight / 2) * divider;
+            layer.style.transform = `translate(${positionX}px, ${positionY}px)`;
+        });
+    }   
+    window.addEventListener('mousemove', moveLayers)
+};
